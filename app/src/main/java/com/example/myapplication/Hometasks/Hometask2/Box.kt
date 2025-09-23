77d1599c103da6c459687c9b15178db5f10f1aa5
peshne-elizaveta <e.peshne@g.nsu.ru>
2025-09-23 11:00:47 +0700

    hello

--- app/src/main/java/com/example/myapplication/Hometasks/Hometask2/Box.kt
+++ app/src/main/java/com/example/myapplication/Hometasks/Hometask2/Box.kt
@@ -1,2 +1,21 @@
 package com.example.myapplication.Hometasks
 
+class Box<T>(private val value: T) {
+
+    fun getValue(): T {
+        return value
+    }
+
+    fun <R> map(new: (T) -> R): Box<R> = Box(new(value))
+}
+
+fun main() {
+    //Int -> String
+    val intBox = Box(459)
+    val stringBox: Box<String> = intBox.map { it.toString() }
+    println("String value: ${stringBox.getValue()}")
+
+    //Int -> Boolean
+    val boolBox: Box<Boolean> = intBox.map { it > 0 }
+    println("Boolean value: ${boolBox.getValue()}")
+}
